bfe2b70bc5555fb87b1654b053df5b9dc48c71d6
ThaseG <53595763+ThaseG@users.noreply.github.com>
2026-04-26 10:36:24 +0200

    V1.1.4 (#4)
    OpenVPN Version: 2.7.2
    OpenVPN Exporter Version: 1.1.1
    golang Version: 1.25.8
    Ubuntu Version: 24.04
    
    Upgrade of OpenVPN version, that fixes CVEs:
    
    CVE-2026-40215: fix race condition in TLS handshake that could lead to leaking of
    packet data from a previous handshake under specific circumstances
    CVE-2026-35058: fix server ASSERT() on receiving a suitably malformed packet with
    a valid tls-crypt-v2 key

--- versions.sh
+++ versions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE_VERSION='v1.1.3'
-OPENVPN_VERSION='v2.7.1' # For upgrade, please update also value in server/openvpn.dockerfile
+IMAGE_VERSION='v1.1.4'
+OPENVPN_VERSION='v2.7.2' # For upgrade, please update also value in server/openvpn.dockerfile
 CLIENT_IMAGE_VERSIONS=("bullseye" "bookworm" "jammy")

9b3d98fd46d149a92e9a87d9c16ddb87d241948f
ThaseG <andrej@hyben.net>
2026-04-28 13:51:44 +0200

    v1.1.5

--- versions.sh
+++ versions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE_VERSION='v1.1.4'
-OPENVPN_VERSION='v2.7.2' # For upgrade, please update also value in server/openvpn.dockerfile
+IMAGE_VERSION='v1.1.5'
+OPENVPN_VERSION='v2.7.3' # For upgrade, please update also value in server/openvpn.dockerfile
 CLIENT_IMAGE_VERSIONS=("bullseye" "bookworm" "jammy")
